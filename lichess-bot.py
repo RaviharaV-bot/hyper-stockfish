e497fb7202e3883ce3e1887023c130e1bd35afcf
RaviharaV <91451271+RaviharaV-bot@users.noreply.github.com>
2022-01-02 12:10:12 +0530

    Update lichess-bot.py

--- lichess-bot.py
+++ lichess-bot.py
@@ -250,16 +250,16 @@ def play_game(li, game_id, control_queue, engine_factory, user_profile, config, 
     conversation.send_reply(SendLine('player'), f'you can also play with @lila-stockfish')
     conversation.send_reply(SendLine('spectator'), f'Welcome to my game spectators!')
     conversation.send_reply(SendLine('spectator'), f'This is a bot created by @RaviharaV you can also watch @lila-stockfish games')
+    
+    board = setup_board(game)
+    cfg = config["engine"]
      
     variant=game.perf_name
     
     if variant=="standard":
         engine_path = os.path.join(cfg["dir"], cfg["sfname"])
         engineeng = engine.SimpleEngine.popen_uci(engine_path) 
-        
-    board = setup_board(game)
-    cfg = config["engine"]
- 
+         
     logger.info("+++ {}".format(game))
 
     is_correspondence = game.perf_name == "Correspondence"
