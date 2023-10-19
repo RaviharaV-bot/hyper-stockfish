b5146842eca83ea69cc0a76d6aebd5f5e0ee4b70
RaviharaV <91451271+RaviharaV-bot@users.noreply.github.com>
2023-10-20 01:56:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-su apt install -y p7zip-full
-su apt update && apt install -y wget python3 python3-pip p7zip-full git
+#su apt install -y p7zip-full
+#su apt update && apt install -y wget python3 python3-pip p7zip-full git
 python3 -m pip install --no-cache-dir -r requirements.txt
 git clone https://github.com/hyperbotauthor/syzygy.git
-wget --no-check-certificate -nv "https://gitlab.com/OIVAS7572/Goi5.1.bin/-/raw/MEGA/Goi5.1.bin.7z" -O Goi5.1.bin.7z \
-&& 7z e Goi5.1.bin.7z && rm Goi5.1.bin.7z
+wget --no-check-certificate -nv "https://gitlab.com/OIVAS7572/Goi5.1.bin/-/raw/MEGA/Goi5.1.bin.7z" -O Goi5.1.bin.7z 
+#&& 7z e Goi5.1.bin.7z && rm Goi5.1.bin.7z
+unzip Goi5.1.bin.7z
 bash multifish.sh
 python3 lichess-bot.py -u
